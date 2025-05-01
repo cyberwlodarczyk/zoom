b692eef168525e29bae3af68fc2805f7471c5810
Łukasz Włodarczyk <cyberwlodarczyk@proton.me>
2025-05-01 16:25:15 +0200

    basic websocket communication

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,35 @@
+use axum::{
+    Router,
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::Response,
+    routing::get,
+};
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, Tokio!");
+    let app = Router::new().route("/ws", get(handler));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    println!("connection established");
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            return;
+        };
+        if let Message::Text(text) = msg {
+            println!("message received: {}", text);
+            if socket.send(Message::Text(text)).await.is_err() {
+                return;
+            }
+        }
+    }
 }
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello, Vite!");
+const ws = new WebSocket("http://localhost:3000/ws");
+
+ws.addEventListener("open", () => {
+  console.log("connection established");
+  let x = 1;
+  let id = setInterval(() => {
+    const msg = `${x}`;
+    ws.send(msg);
+    console.log(`message sent: ${msg}`);
+    if (x === 5) {
+      clearInterval(id);
+      ws.close();
+    }
+    x++;
+  }, 500);
+});
+
+ws.addEventListener("message", (event) => {
+  console.log(`message received: ${event.data}`);
+});
+
+ws.addEventListener("close", () => {
+  console.log("connection closed");
+});
